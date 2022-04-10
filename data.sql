5dd85326e6e23428306ca936d79695e0d4bcdbcf
Diego Infanta Zeballos <diego@MacBook-Pro-de-Diego.local>
2022-04-09 21:22:47 -0400

    v2

---
+++ data.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE skatepark;
+
+CREATE TABLE skaters (id SERIAL, email VARCHAR(255) NOT NULL, nombre
+VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, anos_experiencia
+INT NOT NULL, especialidad VARCHAR(255) NOT NULL, foto VARCHAR(255) NOT
+NULL, estado BOOLEAN NOT NULL);
--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const app = express()
 const { create } = require('express-handlebars')
 const PORT = process.env.PORT || 3000
 
-//habilitar el req.body
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'))
 
-//habilitar handlebars
 const hbs = create({
     partialsDir: ["views/components"],
     extname: ".hbs"
